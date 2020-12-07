092e10129f4d119da7c8a71c505bb601fa8ec96d
yarim1 <romain.bry@epitech.eu>
2020-12-07 16:50:04 +0100

    testing

--- data/scrapping/scrapping/spiders/test_scr.py
+++ data/scrapping/scrapping/spiders/test_scr.py
@@ -2,37 +2,21 @@
 
 import scrapy
 
-# class TestSpider(scrapy.Spider):
-#     name = "quotes"
-
-#     def start_request(self):
-#         urls = [
-#             'http://nature.jardin.free.fr/2015/citrus-australasica.html',
-#             'http://quotes.toscrape.com/page/2/',
-#         ]
-#         for url in urls:
-#             yield scrapy.Request(url=url, callback=self.parse)
-
-#     def parse(self, response):
-#         self.log(response.body)
-#         page = response.url.split("/")[-2]
-#         filename = f'quotes-{page}.html'
-#         with open(filename, 'wb') as f:
-#             f.write(response.body)
-#         self.log(f'Saved file {filename}')
-
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
-        'http://nature.jardin.free.fr/2015/citrus-australasica.html',
-        # 'http://quotes.toscrape.com/page/1/',
-        # 'http://quotes.toscrape.com/page/2/',
+        'https://www.aujardin.info/plantes/ananas_bracteatus.php',
     ]
 
     def parse(self, response):
-        f = open('testfile2.html', 'w+')
-        for quote in response.css('font'):
-            f.write(quote.get())
+        f = open('testfile.html', 'w+')
+        f.write(response.css('article').get())
+        # for b_tag in response.css('b'):
+        #     f.write(b_tag.xpath('text()').get())
+        #     f.write("".join(str(b_tag.xpath('./following-sibling::text()').extract())) + '\n')
+        #     f.write("".join(str(b_tag.xpath('./following-sibling::i').extract())) + '\n')
+        #     f.write(b_tag.xpath('./following-sibling::i').get().strip() + '\n')
+        f.close()
 
         # for quote in response.css('div.quote'):
         #     yield {
@@ -40,4 +24,3 @@ class QuotesSpider(scrapy.Spider):
         #         'author': quote.css('small.author::text').get(),
         #         'tags': quote.css('div.tags a.tag::text').getall(),
         #     }
-        f.close()
