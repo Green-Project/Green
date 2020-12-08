77b91af28366c2d78299b66a704127a02bd8b1ac
Yarim1 <46965839+Yakai-Sora@users.noreply.github.com>
2020-12-08 12:17:20 +0100

    get data

--- data/scrapping/scrapping/spiders/test_scr.py
+++ data/scrapping/scrapping/spiders/test_scr.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
 
 import scrapy
+import json
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
         'https://www.aujardin.info/plantes/guzmania.php',
+        'https://www.aujardin.info/plantes/ananas_bracteatus.php',
     ]
 
     def parse(self, response):
-        f = open('testfile2.html', 'w+')
-        for attr in response.css('div.description3'):
-            inside = attr.xpath('./child::div')
-            f.write("".join(inside.extract()))
-            # for elem in inside:
-            #     f.write("\n".join(elem.get()))
-            # f.write("\n".join(inside.xpath('./child::strong').extract()))
-            # f.write("\n".join(inside.xpath('./child::span').extract()))
-            # yield {
-            #     'N. Scientifique': elem.css('div[1]').get()
-            # }
-        f.close()
+        d = dict()
+        # f = open('testfile.txt', 'w+')
+        for attr in response.css('div.description3').xpath('./child::div'):
+            # Get tags corresponding to the data we are looking for
+            attr_tags = attr.xpath('./strong')
+            # Extract data from the tags
+            for n in attr_tags:
+                # f.write(str(n.xpath('./text()').extract()[0]) + ' : ')
+                tab = n.xpath('./following-sibling::span/text()')
+                if (len(tab) == 0):
+                    tab = n.xpath('./following-sibling::span/*/text()')
+                # f.write(str(tab.extract()) + '\n')
+                d[str(n.xpath('./text()').extract()[0])] = tab.extract() if len(tab) > 1 else tab[0].extract()
+        with open("plants.json", "a+", encoding='utf-8') as e :
+            json.dump(d, e, ensure_ascii=False, indent=2)
+        # f.close()
