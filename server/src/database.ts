78576097c91b5adf121f0494b6b6aca527798ef3
Vincent <vincent1.son@epitech.eu>
2021-06-06 22:44:50 +0200

    added a .env file to switch between databases. Will certainly be useful for other cases

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,10 +1,13 @@
 import { Bson, MongoClient } from "https://deno.land/x/mongo@v0.23.1/mod.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+
+console.log(config().DB);
 
 const client = new MongoClient();
 
 await client.connect("mongodb://127.0.0.1:27017");
 
-const db = client.database("Green");
+const db = client.database(config().DB);
 
 export interface UserSchema {
     _id: { $oid: string };
--- server/tests/authentication.test.ts
+++ server/tests/authentication.test.ts
@@ -1,12 +1,12 @@
 import { assertEquals } from "https://deno.land/std@0.97.0/testing/asserts.ts";
-import { db, UserSchema } from "./testing_database.ts";
+import { db, UserSchema } from "../src/database.ts";
 import { signup, login} from "../src/controllers/authController.ts"
 
 Deno.test("It creates a new user", async() => {
-    const start = await db.collection<UserSchema>("userTest").find();
+    const start = await db.collection<UserSchema>("user").find();
     assertEquals((await start.toArray()).length, 0);
     assertEquals(await signup("TestUser", "TestPassword"), true);
-    const end = await db.collection<UserSchema>("userTest").find()
+    const end = await db.collection<UserSchema>("user").find()
     assertEquals((await end.toArray()).length, 1);
     console.log(await end.toArray());
 });
